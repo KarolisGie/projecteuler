78212af2de32b7bf94d921a05805157e11cfe76a
KarolisGie <karolis.giedraitis@gmail.com>
2015-09-02 19:47:04 +0300

    Factorial digit sum

---
+++ 20.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Factorial digit sum
+ * 
+ * n! means n × (n − 1) × ... × 3 × 2 × 1
+ * 
+ * For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+ * and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+ * 
+ * Find the sum of the digits in the number 100!
+ */
+
+$factorial = gmp_strval( gmp_fact( 100 ) );
+$numbers_arr = array();
+
+while ($factorial > 0) {
+	$numbers_arr[] = gmp_strval( gmp_mod( $factorial, 10 ) );
+	$factorial = gmp_strval( gmp_div_q( $factorial, 10 ) );
+}
+
+echo array_sum( $numbers_arr );
